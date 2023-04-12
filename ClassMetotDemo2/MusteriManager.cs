ff17be9d4615716ec0abdba7a8c46794ca54b5a1
Emre Arslan <arslann1907@hotmail.com>
2023-04-12 02:51:31 +0200

    Ders3 sonu 12-04-23

---
+++ ClassMetotDemo2/MusteriManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo2
+{
+    internal class MusteriManager
+    {
+        public void MusteriEkleme(Musteri musteri)
+        {
+            Console.WriteLine("Musteri: " + musteri.Ad + " " + musteri.Soyad + " Eklendi.");
+        }
+
+        public void MusteriListeleme(Musteri musteri)
+        {
+            Console.WriteLine("Musteri: " + musteri.Ad + " " + musteri.Soyad + ".");
+        }
+
+        public void MusteriSilme(Musteri musteri)
+        {
+            Console.WriteLine("Musteri: " + musteri.Ad + " " + musteri.Soyad + " Silindi.");
+        }
+    }
+}
---
+++ ClassMetotDemo2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ClassMetotDemo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Ad = "Mehmet";
+            musteri1.Soyad = "Uçar";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Ad = "Ismet";
+            musteri2.Soyad = "Duser";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2 };
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine("Musteri bilgisi: " + musteri.Ad + " " + musteri.Soyad);
+                Console.WriteLine("Musteri Numarasi: " + musteri.Id);
+            }
+        }
+    }
+}
